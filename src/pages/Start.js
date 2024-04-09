79aa5b18a36d32bef7e012effdfd906bdb1f4d32
Milorad <145986241+Mugen1988@users.noreply.github.com>
2024-04-09 14:32:25 +0200

    new features
    Added new features for start new game and selecting difficulty level

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,15 +1,60 @@
 import React from "react"
 
-export function Start({startGame}){
+export function Start({ startGame }) {
+    const [selectedDifficulty, setSelectedDifficulty] = React.useState("easy");
+  
+    const handleChange = (event) => {
+      setSelectedDifficulty(event.target.value);
+    };
+  
+    const handleClick = () => {
+      startGame(selectedDifficulty);
+    };
+  
     return (
         <div className="start-screen">
-            <h1 className="start-title">Quizzical</h1>
-            <h2 className="start-subtitle">
-                Stretch Your Brain - Trivia that Spans from Shakespeare to Sharknado!
-            </h2>
-            <button className="start-button" onClick={startGame}>
-                Start Quiz
-            </button>
+          <h1 className="start-title">Quizzical</h1>
+          <h2 className="start-subtitle">
+            Stretch Your Brain - Trivia that Spans from Shakespeare to Sharknado!
+          </h2>
+          <div className="radio-container">
+            <input
+              type="radio"
+              id="easy"
+              value="easy"
+              checked={selectedDifficulty === "easy"}
+              onChange={handleChange}
+              className="radio-input"
+            />
+            <label htmlFor="easy" className="radio-button">
+              Easy
+            </label>
+            <input
+              type="radio"
+              id="medium"
+              value="medium"
+              checked={selectedDifficulty === "medium"}
+              onChange={handleChange}
+              className="radio-input"
+            />
+            <label htmlFor="medium" className="radio-button">
+              Medium
+            </label>
+            <input
+              type="radio"
+              id="hard"
+              value="hard"
+              checked={selectedDifficulty === "hard"}
+              onChange={handleChange}
+              className="radio-input"
+            />
+            <label htmlFor="hard" className="radio-button">
+              Hard
+            </label>
+          </div>
+          <button className="start-button" onClick={handleClick}>
+            Start Quiz
+          </button>
         </div>
-    )
+      );
 }
